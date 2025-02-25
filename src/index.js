24d0c9941a8ff4bfe4ba7008d3b4133857a4009f
Bradley Morin <bradleymorin@gmail.com>
2025-02-25 16:27:51 -0700

    Add Gameboard object

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import "./style.css";
 
-class Ship {
-    constructor(length = 2) {
-        this.hits = 0;
-        this.length = length;
-    }
+export class Ship {
+  constructor(length = 2, orientation = "stern left") {
+    this.hits = 0;
+    this.length = length;
+    this.orientation = orientation;
+  }
 
-    hit() {
-        this.hits++;
-    }
+  hit() {
+    this.hits++;
+  }
 
-    isSunk() {
-        if (this.hits >= this.length) {
-            return true;
+  isSunk() {
+    if (this.hits >= this.length) {
+      return true;
+    }
+    return false;
+  }
+}
+
+export class Gameboard {
+    constructor(length = 9, width = 9) {
+        this.rows = length;
+        this.columns = width;
+        this.board = [];
+
+        for (let i = 0; i < this.rows; i++) {
+            this.board[i] = [];
+            for (let j = 0; j < this.columns; j++) {
+                this.board[i].push(new Cell());
+            }
         }
-        return false;
     }
 }
 
-export default Ship;
+export class Cell {
+    constructor() {
+        this.testProperty = "";
+    }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,19 +1,46 @@
-import Ship from "./index.js";
+import { Ship, Gameboard, Cell } from "./index.js";
 
-test('Hitting a ship increments hit counter', () => {
+describe("Ship object tests", () => {
+  test("ship.hits is initialized to zero.", () => {
+    const ship = new Ship();
+    expect(ship.hits).toBe(0);
+  });
+
+  test("ship.hit() increments ship.hits counter.", () => {
     const ship = new Ship();
     ship.hit();
     expect(ship.hits).toBe(1);
-})
+  });
 
-test('New ships have are not sunk', () => {
+  test("ship.length is initialized via constructor", () => {
+    const length = 4;
+    const ship = new Ship(length);
+    expect(ship.length).toBe(length);
+  });
+
+  test("isSunk is false on initialization.", () => {
     const ship = new Ship();
     expect(ship.isSunk()).toBe(false);
-})
+  });
 
-test('Enough hits sink ships', () => {
+  test("isSunk is true when the ship's been hit enough", () => {
     const ship = new Ship(2);
     ship.hit();
     ship.hit();
     expect(ship.isSunk()).toBe(true);
+  });
+});
+
+describe("Gameboard object tests", () => {
+    test("Gameboard is initialized with Cell objects.", () => {
+        const game = new Gameboard();
+        expect(game.board[0][0]).toBeInstanceOf(Cell);
+    })
+})
+
+describe("Cell object tests", () => {
+    test("Test property is initialized", () => {
+        const cell = new Cell();
+        expect(cell).toHaveProperty("testProperty");
+    })
 })
