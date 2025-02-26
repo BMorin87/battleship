18138416fbc39d88e99a0c5505b8fde021855346
Bradley Morin <bradleymorin@gmail.com>
2025-02-25 18:36:53 -0700

    placeShips WIP

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ export class Ship {
   constructor(length = 2, orientation = "stern left") {
     this.hits = 0;
     this.length = length;
+    // Ships are oriented with their stern pointing either left or up.
     this.orientation = orientation;
   }
 
   hit() {
     this.hits++;
+  }
+
+  setLocation(coordinates) {
+    this.location = coordinates;
   }
 
   isSunk() {
@@ -29,6 +34,7 @@ export class Gameboard {
     
     this.createBoard();
     this.createShips();
+    this.placeShips();
   }
 
   createBoard() {
@@ -47,10 +53,31 @@ export class Gameboard {
       }
     })
   }
+
+  placeShips() {
+    this.players.forEach(player => {
+      player.ships.forEach(ship => {
+        const place = this.getFirstLegalPosition(ship);
+        ship.setLocation(place);
+        // TODO: Set board cells to Ship type.
+      })
+    })
+  }
+
+  getFirstLegalPosition(ship) {
+    // Find the first cell where the ship fits.
+    // A ship can be oriented with its stern pointing left or up.
+    // A ship's location refers only to the cell containing the ship's stern.
+    // Find the first cell that has ship.length free tiles to the right or below.
+  }
 }
 
 export class Cell {
   constructor(type = "Ocean") {
+    this.setType(type);
+  }
+
+  setType(type) {
     this.type = type;
   }
 }
