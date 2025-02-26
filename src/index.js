24cd408474e8f9efa140f04ee049ffb432f475d9
Bradley Morin <bradleymorin@gmail.com>
2025-02-25 18:19:44 -0700

    Gameboard creates ships

--- src/index.js
+++ src/index.js
@@ -20,22 +20,44 @@ export class Ship {
 }
 
 export class Gameboard {
-    constructor(length = 9, width = 9) {
-        this.rows = length;
-        this.columns = width;
-        this.board = [];
+  constructor(length = 9, width = 9, shipCount = 5) {
+    this.rows = length;
+    this.columns = width;
+    this.players = [new Player(), new Player()];
+    this.shipCount = shipCount;
+    this.board = [];
+    
+    this.createBoard();
+    this.createShips();
+  }
 
-        for (let i = 0; i < this.rows; i++) {
-            this.board[i] = [];
-            for (let j = 0; j < this.columns; j++) {
-                this.board[i].push(new Cell());
-            }
-        }
+  createBoard() {
+    for (let i = 0; i < this.rows; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < this.columns; j++) {
+        this.board[i].push(new Cell());
+      }
     }
+  }
+
+  createShips() {
+    this.players.forEach(player => {
+      for (let i = 0; i < this.shipCount; i++) {
+        player.ships.push(new Ship());
+      }
+    })
+  }
 }
 
 export class Cell {
-    constructor() {
-        this.testProperty = "";
-    }
+  constructor(type = "Ocean") {
+    this.type = type;
+  }
+}
+
+export class Player {
+  constructor(playerType = "Human") {
+    this.type = playerType;
+    this.ships = [];
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
-import { Ship, Gameboard, Cell } from "./index.js";
+import { Ship, Gameboard, Cell, Player } from "./index.js";
 
-describe("Ship object tests", () => {
+describe("Ship tests", () => {
   test("ship.hits is initialized to zero.", () => {
     const ship = new Ship();
     expect(ship.hits).toBe(0);
@@ -12,7 +12,7 @@ describe("Ship object tests", () => {
     expect(ship.hits).toBe(1);
   });
 
-  test("ship.length is initialized via constructor", () => {
+  test("ship.length is initialized via constructor.", () => {
     const length = 4;
     const ship = new Ship(length);
     expect(ship.length).toBe(length);
@@ -23,7 +23,7 @@ describe("Ship object tests", () => {
     expect(ship.isSunk()).toBe(false);
   });
 
-  test("isSunk is true when the ship's been hit enough", () => {
+  test("isSunk is true when the ship's been hit enough.", () => {
     const ship = new Ship(2);
     ship.hit();
     ship.hit();
@@ -31,16 +31,61 @@ describe("Ship object tests", () => {
   });
 });
 
-describe("Gameboard object tests", () => {
-    test("Gameboard is initialized with Cell objects.", () => {
-        const game = new Gameboard();
-        expect(game.board[0][0]).toBeInstanceOf(Cell);
-    })
-})
+describe("Gameboard tests", () => {
+  test("Gameboard is initialized with Cell objects.", () => {
+    const game = new Gameboard();
+    expect(game.board[0][0]).toBeInstanceOf(Cell);
+  });
 
-describe("Cell object tests", () => {
-    test("Test property is initialized", () => {
-        const cell = new Cell();
-        expect(cell).toHaveProperty("testProperty");
-    })
-})
+  test("Gameboard Cells default type is Ocean.", () => {
+    const game = new Gameboard();
+    const isAllOceans = game.board.every((row) =>
+      row.every((cell) => cell.type === "Ocean")
+    );
+    expect(isAllOceans).toBe(true);
+  });
+
+  test("Gameboard creates ships for each player.", () => {
+    const game = new Gameboard();
+    const isAllShips = game.players.every(player =>
+      player.ships.every(ship => ship instanceof Ship)
+    );
+    expect(isAllShips).toBe(true);
+  });
+
+  test("Gameboard creates shipCount number of ships for each player.", () => {
+    const game = new Gameboard();
+    expect(game.players[0].ships.length).toBe(game.shipCount);
+  });
+});
+
+describe("Cell tests", () => {
+  test("Default cell type is Ocean.", () => {
+    const cell = new Cell();
+    expect(cell.type).toBe("Ocean");
+  });
+
+  test("Cell type is set via constructor.", () => {
+    const testType = "Ship";
+    const cell = new Cell(testType);
+    expect(cell.type).toBe(testType);
+  });
+});
+
+describe("Player tests", () => {
+  test("Player object is created with a type property.", () => {
+    const player = new Player();
+    expect(player).toHaveProperty("type");
+  });
+
+  test("Player object is created with a ships property.", () => {
+    const player = new Player();
+    expect(player).toHaveProperty("ships");
+  });
+
+  test("player.type is set via constructor.", () => {
+    const testType = "CPU";
+    const player = new Player(testType);
+    expect(player.type).toBe(testType);
+  });
+});
